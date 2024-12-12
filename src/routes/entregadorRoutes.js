c55f2521027c07dd4357b5430eadb555d2ba5ae3
BurningHades01 <erickambrosioking@gmail.com>
2024-12-12 09:12:27 -0300

    entregadorController

--- src/routes/entregadorRoutes.js
+++ src/routes/entregadorRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const entregadorController = require('../controller/entregadorController')
 
-router.get('/entregador', entregadorController.listarEntregador);
-router.get('/entregador/:codigoEntregador', entregadorController.listarEntregadorCpf);
-router.get('/entregador/nome/:nomeEntregador', entregadorController.nomeEntregador);
+ router.get('/entregador', entregadorController.listarEntregador);
+ router.get('/entregador/:codigoEntregador', entregadorController.listarEntregadorCpf);
 router.post('/entregador', entregadorController.adicionarEntregador);
 router.put('/entregador/:codigoEntregador', entregadorController.atualizarEntregador);
 router.delete('/entregador/:codigoEntregador', entregadorController.deletarEntregador);
